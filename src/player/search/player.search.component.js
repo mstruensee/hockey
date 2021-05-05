ee21057a66e06bed37e03b1f20ab1d83b29efb61
Matthew Struensee <matt.struensee@bestbuy.com>
2021-05-05 09:10:56 -0500

    clear search terms on load

--- src/player/search/player.search.component.js
+++ src/player/search/player.search.component.js
@@ -1,16 +1,24 @@
-import React, { Fragment } from "react"
+import React, { useEffect } from "react"
 import {
 	useDispatch,
-	useSelector
+	useSelector,
 } from "react-redux"
 import { SearchComponent } from "../../search/search.component"
 import { setPlayerSearchTermAction } from "./player.search.actions"
 import { playerSearchTermSelector } from "./player.search.selectors"
+import { PLAYER_SEARCH_TERM_INITIAL_STATE } from "./player.search.types"
 
 export const PlayerSearchComponent = () => {
 	const dispatch = useDispatch()
 	const onChange = event => dispatch(setPlayerSearchTermAction(event.target.value))
 	const playerSearchTerm = useSelector(playerSearchTermSelector)
+
+	useEffect(
+		() => {
+			dispatch(setPlayerSearchTermAction(PLAYER_SEARCH_TERM_INITIAL_STATE))
+		},
+		[]
+	)
 
 	return (
 		<SearchComponent
--- src/teams/search/teams.search.component.js
+++ src/teams/search/teams.search.component.js
@@ -1,16 +1,24 @@
-import React, { Fragment } from "react"
+import React, { useEffect } from "react"
 import {
 	useDispatch,
-	useSelector
+	useSelector,
 } from "react-redux"
 import { SearchComponent } from "../../search/search.component"
 import { setTeamsSearchTermAction } from "./teams.search.actions"
 import { teamsSearchTermSelector } from "./teams.search.selectors"
+import { TEAMS_SEARCH_TERM_INITIAL_STATE } from "./teams.search.types"
 
 export const TeamsSearchComponent = () => {
 	const dispatch = useDispatch()
 	const onChange = event => dispatch(setTeamsSearchTermAction(event.target.value))
 	const teamsSearchTerm = useSelector(teamsSearchTermSelector)
+
+	useEffect(
+		() => {
+			dispatch(setTeamsSearchTermAction(TEAMS_SEARCH_TERM_INITIAL_STATE))
+		},
+		[]
+	)
 
 	return (
 		<SearchComponent
